c609834873cbc0fad3c82ec119dae8b45b0b32aa
Sahil Gunake <sahilgunake@gmail.com>
2022-12-07 12:51:20 +0530

    Init commit

---
+++ Tweet-App-Comp3/Backend/TweetApp/MessegeSender/KafkaSender.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace TweetApp.MessegeSender
+{
+    public class KafkaSender : IKafkaSender
+    {
+        async public void Publish(string message)
+        {
+            var conStr = "Your service bus con string";
+            var client = new ServiceBusClient(conStr);
+            var sender = client.CreateSender("Your topic name");
+            var Message = new ServiceBusMessage(message);
+            await sender.SendMessageAsync(Message);
+
+        }
+    }
+}
